268ee0f353025e05eca23d0f7bc7b3d195e6bc1d
LuceroSotelo <lucero.eds@gmail.com>
2022-03-11 19:23:18 -0500

    Entrevista técnica resolución

--- boilerplate/countVowels.js
+++ boilerplate/countVowels.js
@@ -1,5 +1,28 @@
 const countVowels = (str) => {
-  // ...
+
+  let vocalObj = { a, e, i, o,u}
+  const wordSeparate = str.split();
+
+  for (let i = 0; i < wordSeparate.length; i++) {
+    if (wordSeparate[i] == "a") {
+      Object.assign(vocalObj, wordSeparate[i])
+    }
+    if (wordSeparate[i] == "e") {
+      Object.assign(vocalObj, wordSeparate[i])
+    }
+    if (wordSeparate[i] == "i") {
+      Object.assign(vocalObj, wordSeparate[i])
+    }
+    if (wordSeparate[i] == "o") {
+      Object.assign(vocalObj, wordSeparate[i])
+    }
+    if (wordSeparate[i] == "u") {
+      Object.assign(vocalObj, wordSeparate[i])
+    }
+  }
+  return vocalObj
 };
+
+console.log(countVowels)
 
 module.exports = countVowels;
